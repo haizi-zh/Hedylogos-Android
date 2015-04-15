a5ec34c53c3239a6a0c1edd91d7fdcbebf27acfa
wdqyb <29684101@qq.com>
2015-04-15 15:59:03 +0800

    project init

--- sdk/app/src/main/java/com/hedylogos/MainActivity.java
+++ sdk/app/src/main/java/com/hedylogos/MainActivity.java
@@ -1,39 +1,77 @@
 package com.hedylogos;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.hedylogos.net.UploadUtils;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
+    Button btn;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        btn = (Button) findViewById(R.id.btn);
+        img = (ImageView) findViewById(R.id.img);
+        btn.setOnClickListener(this);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == 1) {
+            Log.i("tag", "resultCode == RESULT_OK && requestCode == CHOOSE_IMAGE_CODE");
+            Uri uri = data.getData();
+            ContentResolver cr = this.getContentResolver();
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                uploadBitmap(bitmap);
+                Log.i("tag", "uploadBitmap(bitmap); ");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
-        return super.onOptionsItemSelected(item);
+    /**
+     * 上传从相册选择的图片
+     *
+     * @param result
+     */
+    private void uploadBitmap(Bitmap result) {
+        UploadUtils.getInstance().uploadImage(result, new UploadUtils.QiniuUploadUitlsListener() {
+
+            public void onSucess(String fileUrl) {
+
+                System.out.print("上传图片成功");
+            }
+
+            public void onProgress(int progress) {
+            }
+
+            public void onError(int errorCode, String msg) {
+                System.out.print("errorCode=" + errorCode + ",msg=" + msg);
+            }
+        });
     }
 }
---
+++ sdk/app/src/main/java/com/hedylogos/net/DownloadUtils.java
@@ -0,0 +1,8 @@
+package com.hedylogos.net;
+
+/**
+ * Created by q on 2015/4/14.
+ */
+public class DownloadUtils {
+
+}
